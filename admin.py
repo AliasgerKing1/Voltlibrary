9dcf709bb92b068c7d5231850b37e20b6d6026ce
alihuede <76582089+alihuede@users.noreply.github.com>
2023-02-24 17:32:51 +0530

    completed, may minor things left to implement

--- admin.py
+++ admin.py
@@ -1,90 +1,157 @@
 import os
-print("Login Authentication")
-valid_id = "a"
-# AliasgerB
-valid_pass = "b"
-# -A1li@sge3939
-lim = 3
-auth=False
-while lim!=0:
-    user_id = (input("Enter Username : "))
-    user_pass = (input("Enter password : "))
-    if user_id == valid_id:
-        if user_pass == valid_pass:
-            print("yess")
-            auth=True;
-            break
+while True:
+    print("Login Authentication")
+    valid_id = "AliasgerB"
+    valid_pass = "-A1li@sge3939"
+    lim = 3
+    auth = False
+    while lim != 0:
+        user_id = (input("Enter Username : "))
+        user_pass = (input("Enter password : "))
+        if user_id == valid_id:
+            if user_pass == valid_pass:
+                auth = True
+                break
+            else:
+                print('Incorrect Password')
+                lim -= 1
+                continue
         else:
-            print('Incorrect Password');
-            lim-=1;
+            print('Incorrect Username.')
+            lim -= 1
             continue
-    else:
-        print('Incorrect Username.')
-        lim-=1;
-        continue
-if auth:
-    print("This is the list of all Admin tools")
-    print("1 = create a book\n2 = view all books")
-    tool = int(input("Selected tool : "))
-    if tool == 1 :
-        print(" c = create book\n r = remove book\n mr = remove multiple book")
-        diract = (input("Enter function : "))
-        if diract == "c" :
-            print("out = stop creating book")
-            while True :
-                print("emp = make empty book\nwp = with pages")
-                book_type = input("Selected : ")
-                if book_type == "wp" :
-                    is_exit = input("Are you want to exit write" +"\033[1m" + " out" + "\033[0m" + ": ")
-                    if is_exit == "out" :
-                        break
-                    else :
-                        book_name = (input("Enter book name : "))
-                        path = f"files/{book_name}"
-                        os.mkdir(path)
-                        print("cr = create a page\ne = edit page\no = open page\nrm = remove page\ncp = copy content of page")
-                        fileact = (input("Enter action to perform : "))
-                        if fileact == "cr" :
-                            file_name = (input("Enter file name : "))
-                            content = (input("Enter Content :"))
-                            with open(f"files/{book_name}/{file_name}", "w") as file :
-                                file.write(content)  
-                elif book_type == "emp" :
-                    is_exit = input("Are you want to exit write" +"\033[1m" + " out" + "\033[0m" + ": ")
-                    if is_exit == "out" :
-                        break
-                    else :
-                        book_name = (input("Enter book name : "))
-                        path = f"files/{book_name}"
-                        os.mkdir(path)
-        if diract == "r" :
-            rmBook_name = (input("Enter book name : "))
-            delpath = f"files/{rmBook_name}"
-            os.rmdir(delpath)
-        if diract == "mr" :
-            print("out = stop removing book")
-            while True :
-                mlutiRemove = (input("Enter Book name : "))
-                list_file = os.listdir(f"files/{mlutiRemove}")
-                if len(list_file) != 0 :
-                    for lf in list_file :
-                        os.remove(f"files/{mlutiRemove}/{lf}")
-                        os.rmdir(f"files/{mlutiRemove}")
-                        is_exit = input("Are you want to exit write" +"\033[1m" + " out" + "\033[0m" + ": ")
-                        print(is_exit)
-                        if is_exit == "out" :
+    if auth:
+        print("This is the list of all Admin tools")
+        print("1 = create a book\n2 = view all books")
+        tool = int(input("Selected tool : "))
+        if tool == 1:
+            print(" c = create book\n r = remove book\n mr = remove multiple book")
+            diract = (input("Enter function : "))
+            if diract == "c":
+                print("out = stop creating book")
+                while True:
+                    print(
+                        "emp = make empty book\nwp = with pages\neep = edit existing page")
+                    book_type = input("Selected : ")
+                    if book_type == "wp":
+                        is_exit = input("Are you want to exit write" +
+                                        "\033[1m" + " out" + "\033[0m" + ": ")
+                        if is_exit == "out":
                             break
-                    break
-                else :
-                    os.rmdir(f"files/{mlutiRemove}")
+                        else:
+                            book_name = (input("Enter book name : "))
+                            path = f"files/{book_name}"
+                            os.mkdir(path)
+                            print(
+                                "cr = create a page\ne = edit page\no = open page\ncp = copy content of page")
+                            fileact = (input("Enter action to perform : "))
+                            if fileact == "cr":
+                                file_name = (input("Enter page name : "))
+                                content = (input("Enter Content :"))
+                                with open(f"files/{book_name}/{file_name}.txt", "w") as file:
+                                    file.write(content)
+                            elif fileact == "e":
+                                file_name = (input("Enter page name : "))
+                                content = (input("Enter Content :"))
+                                with open(f"files/{book_name}/{file_name}.txt", "a") as file:
+                                    file.write(content)
+                            elif fileact == "o":
+                                read = input("enter page name to view : ")
+                                readFile = f"files/{book_name}/{read}.txt"
+                                for line in readFile:
+                                    print(line.strip())
+                            elif fileact == "rm":
+                                while True:
+                                    is_exit = input("Are you want to exit write" +
+                                                    "\033[1m" + " out" + "\033[0m" + ": ")
+                                    if is_exit == "out":
+                                        break
+                                    else:
+                                        rmPage = input(
+                                            "Enter Page name to remove : ")
+                                        os.remove(rmPage)
+                    elif book_type == "emp":
+                        is_exit = input("Are you want to exit write" +
+                                        "\033[1m" + " out" + "\033[0m" + ": ")
+                        if is_exit == "out":
+                            break
+                        else:
+                            book_name = (input("Enter book name : "))
+                            path = f"files/{book_name}"
+                            os.mkdir(path)
+                    elif book_type == "eep":
+                        print("rm = remove page, o = open page")
+                        removePage = input("select option : ")
+                        if removePage == "rm":
+                            while True:
+                                is_exit = input("Are you want to exit write" +
+                                                "\033[1m" + " out" + "\033[0m" + ": ")
+                                if is_exit == "out":
+                                    break
+                                else:
+                                    rmPage = input("Enter book name : ")
+                                    rmPage2 = input(
+                                        "Enter Page name to remove : ")
+                                    path = f"files/{rmPage}/{rmPage2}"
+                                    os.remove(path)
+                        elif removePage == "o" :
+                            book_n = input("enter book name : ")
+                            read = input("enter page name to view : ")
+                            readFile = f"files/{book_n}/{read}.txt"
+                            for line in readFile:
+                                print(line.strip())
+                        is_exit = input("Are you want to exit write" +
+                                        "\033[1m" + " out" + "\033[0m" + ": ")
+                        if is_exit == "out":
+                            break
+                        else:
+                            book = input("Enter book name: ")
+                            if not book:
+                                print("Book name cannot be empty")
+                                continue
+                            page = input("Enter page name: ")
+                            if not page:
+                                print("Page name cannot be empty")
+                                continue
+                            page_with_path = f"files/{book}/{page}"
+                            try:
+                                with open(page_with_path, 'r') as f:
+                                    for line in f:
+                                        print(line.strip())
+                            except FileNotFoundError:
+                                print(f"File not found: {page_with_path}")
+                            except Exception as e:
+                                print(
+                                    f"Error reading file: {page_with_path} - {e}")
+            if diract == "r":
+                rmBook_name = (input("Enter book name : "))
+                delpath = f"files/{rmBook_name}"
+                os.rmdir(delpath)
+            if diract == "mr":
+                print("out = stop removing book")
+                while True:
+                    mlutiRemove = (input("Enter Book name : "))
+                    list_file = os.listdir(f"files/{mlutiRemove}")
+                    if len(list_file) != 0:
+                        for lf in list_file:
+                            os.remove(f"files/{mlutiRemove}/{lf}")
+                            os.rmdir(f"files/{mlutiRemove}")
+                            is_exit = input(
+                                "Are you want to exit write" + "\033[1m" + " out" + "\033[0m" + ": ")
+                            print(is_exit)
+                            if is_exit == "out":
+                                break
+                        break
+                    else:
+                        os.rmdir(f"files/{mlutiRemove}")
+        if tool == 2 :
+            lst = os.listdir("files")
+            for i in lst :
+                print(i)
+                print("-------------------------------------------------------------")
+    else:
+        print("Too many attempts")
 
-else :
-    print("Too many attempts")
-
-
-
-
-
-            # content = (input("Enter Content :"))
-        # with open("files/" + book_name, "w") as file :
-            # file.write(content)  
+    # content = (input("Enter Content :"))
+    # with open("files/" + book_name, "w") as file :
+    # file.write(content)
--- user.py
+++ user.py
@@ -1,25 +1,62 @@
-print("Login Authentication")
-valid_id = "AliasgerB"
-valid_pass = "-A1li@sge3939"
-user_id = str(input("Enter Username : "))
-user_pass = str(input("Enter password : "))
-u_id = False
-u_pass = False
-if user_id == valid_id :
-    if user_pass == valid_pass :
-        print("true")
-else :
-    if user_id != valid_id :
-        user_id = str(input("Enter Username : "))
-        if user_id == valid_id :
-            u_id = True
-    else :
-        print("password is incorrect!")
-    if user_pass != valid_pass :
-            user_pass = str(input("Enter password : "))
-            if user_id == valid_id :
-                u_pass = True
-    else :
-        print("Username is incorrect!")
-if u_id == True and u_pass ==  True :
-    print("true")
+import os
+
+while True:
+    print("log = Login\nreg = register")
+    mode = input("Enter mode : ")
+    if mode == "reg":
+        print("\033[1m" + "Registration : " + "\033[0m")
+        user_id = input("Enter your username: ")
+        user_pass = input("Enter your password: ")
+        with open("logindetails/login", "a") as file:
+            file.write(f"{user_id}::{user_pass}\n")
+    if mode == "log":
+        print("Login Authentication")
+        valid_ids = []
+        valid_passwords = []
+        with open("logindetails/login", "r") as file:
+            for line in file:
+                user_id, user_pass = line.strip().split("::")
+                valid_ids.append(user_id)
+                valid_passwords.append(user_pass)
+        lim = 3
+        auth = False
+        while lim != 0:
+            user_id = input("Enter username: ")
+            user_pass = input("Enter password: ")
+            if user_id in valid_ids:
+                index = valid_ids.index(user_id)
+                if user_pass == valid_passwords[index]:
+                    auth = True
+                    break
+                else:
+                    print('Incorrect Password.')
+                    lim -= 1
+                    continue
+            else:
+                print('Incorrect Username.')
+                lim -= 1
+                continue
+        if auth:
+            print("Authentication successful")
+            ubook_name = input("enter book name tha you want to read: ")
+            book_item_list = f"files/{ubook_name}"
+            page_name = input("Enter page name: ")
+            full_page = f"{page_name}.txt"
+            lst = os.listdir(book_item_list)
+            found = False
+
+            for file in lst:
+                if file == full_page:
+                    with open(os.path.join(book_item_list, full_page), "r") as f:
+                        content = f.read()
+                        print(f"{page_name}:")
+                        print(content)
+                        found = True
+                        break
+
+            if not found:
+                print("Page not found.")
+        else:
+            print("Invalid selection.")
+    else:
+        print("Too many attempts")
