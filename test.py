508148bf701e8ea27f83373fd440264d2e57c6da
AliasgerKing1 <aliasgersub34.com>
2023-03-02 13:01:50 +0530

    fix area images

--- test.py
+++ test.py
@@ -2,20 +2,21 @@ import os
 import requests
 from bs4 import BeautifulSoup
 import base64
+from urllib.parse import urljoin
+import time
 
 # Prompt user for website link 
-link = str(input("Enter Link: "))
+link = input("Enter website link: ")
+
+httpCount, dataCount, serverCount = 0, 0, 0
 
 # Prompt user for folder name
 folder_name = input("Enter folder name: ")
-folder_path = os.path.join("images", folder_name)
+folder_path = os.path.abspath(os.path.join("images", folder_name))
 
 # Create folder if it doesn't exist
 if not os.path.exists(folder_path):
     os.makedirs(folder_path)
-
-# Prompt user for max number of images to download
-max_images = int(input("Enter max number of images to download: "))
 
 # Get list of already downloaded images
 downloaded_images = os.listdir(folder_path)
@@ -29,36 +30,79 @@ for image in downloaded_images:
         pass
 
 url = link
-response = requests.get(url)
+try:
+    response = requests.get(url)
+    response.raise_for_status()
+except requests.exceptions.HTTPError as http_err:
+    print(f"HTTP error occurred: {http_err}")
+    exit()
+except Exception as err:
+    print(f"An error occurred: {err}")
+    exit()
+
 html_content = response.content
 
 soup = BeautifulSoup(html_content, 'html.parser')
-img_tags = soup.find_all("img")
+div_by_data_test = None
 
-count = last_index + 1
-for img_tag in img_tags:
-    if count > max_images:
+for x in soup.find_all("div"):
+    if x.get("data-test") == "search-photos-route":
+        div_by_data_test = x
+        count = last_index + 1
+        img_tags = x.find_all("img")
+        for img_tag in img_tags:
+            src = img_tag.get("src")
+            if src:
+                if src and src.startswith("http"):
+                    httpCount +=1
+                    full_src = src
+                elif src.startswith("data:image/"):
+                    dataCount +=1
+                    # Base64-encoded image data
+                    parts = src.split(",")
+                    data = parts[1]
+                    ext = parts[0].split(";")[0][len("data:image/"):]
+                    filename = time.strftime("%Y%m%d-%H%M%S") + f"_image{count}.{ext}"
+                    file_path = os.path.abspath(os.path.join(folder_path, filename))
+                    try:
+                        with open(file_path, "wb") as f:
+                            f.write(base64.b64decode(data))
+                    except Exception as err:
+                        print(f"An error occurred while saving {filename}: {err}")
+                    else:
+                        print(f"Saved {filename}")
+                        count += 1
+                    continue
+                else:
+                    serverCount += 1
+                    full_src = urljoin(url, src)
+
+                try:
+                    response2 = requests.get(full_src)
+                    response2.raise_for_status()
+                except requests.exceptions.HTTPError as http_err:
+                    print(f"HTTP error occurred: {http_err}")
+                    continue
+                except Exception as err:
+                    print(f"An error occurred: {err}")
+                    continue
+
+                filename = time.strftime("%Y%m%d-%H%M%S") + f"_image{count}.jpg"
+                # print(filename)
+                file_path = os.path.abspath(os.path.join(folder_path, filename))
+                try:
+                    with open(file_path, "wb") as f:
+                        f.write(response2.content)
+                except Exception as err:
+                    print(f"An error occurred while saving {filename}: {err}")
+                else:
+                    print(f"Saved {filename}")
+                    count += 1
+                    
+                       
+
+                    
+        print(httpCount)
+        print(dataCount)
+        print(serverCount)
         break
-        
-    src = img_tag.get("src")
-    if src and src.startswith("http"):
-        if src.startswith("data:image/"):
-            # Base64-encoded image data
-            parts = src.split(",")
-            data = parts[1]
-            ext = parts[0].split(";")[0][len("data:image/"):]
-            filename = f"image{count}.{ext}"
-            file_path = os.path.join(folder_path, filename)
-            with open(file_path, "wb") as f:
-                f.write(base64.b64decode(data))
-        else:
-            # Image URL
-            response2 = requests.get(src)
-            ext = src.split(".")[-1].lower()
-            if ext in ["jpg", "jpeg", "png", "gif", "webp", "tiff", "psd", "raw", "bmp", "heif", "indd", "svg", "ai", "eps", "pdf"]:
-                filename = f"image{count}.{ext}"
-                file_path = os.path.join(folder_path, filename)
-                with open(file_path, "wb") as f:
-                    f.write(response2.content)
-        print(f"Saved {filename}")
-        count += 1
